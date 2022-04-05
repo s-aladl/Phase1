17a1ece0decbcb3820f767231b64e56a96241f82
Connie3338 <90847632+Connie3338@users.noreply.github.com>
2022-04-05 01:29:47 -0400

    Revert "Revert "shooting, levels""
    This reverts commit 1c913caff1a963f68dda4bcdc04a49c62a18f272.

--- Assets/_Scripts/HeadDetectScript.cs
+++ Assets/_Scripts/HeadDetectScript.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class HeadDetectScript : MonoBehaviour
 {
+    public int killPoints=2;
     GameObject Enemy;
     // Start is called before the first frame update
     void Start()
@@ -12,13 +14,25 @@ public class HeadDetectScript : MonoBehaviour
         
     }
 
-   private void OnCollisionEnter2D(Collision2D collision) 
+    void OnCollisionEnter2D(Collision2D collision) 
     {
         GetComponent<Collider2D>().enabled = false; 
         Enemy.GetComponent<SpriteRenderer>().flipY = true; 
         Enemy.GetComponent<Collider2D>().enabled = false; 
         Vector3 movement = new Vector3(Random.Range(-40,-70), Random.Range(-400,-500));
         Enemy.transform.position += movement * Time.deltaTime; 
+        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            
+                player.Points(killPoints);
+        
+        
+    
     }
+   
+    
+       
+
+
+    
     
 }
